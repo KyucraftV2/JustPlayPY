b9ec4910a46c7b7a835ff439040eb81ecf531ae4
Kyucraft <42reki@gmail.com>
2020-11-12 15:15:33 +0100

    Summary

---
+++ _class_.py
@@ -0,0 +1,47 @@
+class ClasseJ:
+
+    def __init__(self,basepv,arme,baseforce):
+        self.basepv=basepv
+        self.arme = Arme
+        self.baseforce = baseforce
+
+
+
+class Arme:
+
+    def __init__(self,nom,degats,portee):
+        self._nom = nom
+        self._degats = degats
+        self.portee = portee
+
+    def get_nom(self):
+        return self._nom
+
+    def set_nom(self, val):
+        self._nom = val
+
+    nom=property(get_nom, set_nom)
+
+
+    def get_degats(self):
+        return self._degats
+
+    def set_degats(self,val):
+        self._degats = val
+
+    degats = property(get_degats,set_degats)
+
+class Joueur:
+
+    def __init(self,pv,classe,coords):
+        self._pv = pv
+        self.classe = ClasseJ
+        self.coords = coords
+
+    def get_pv(self):
+        return self._pv
+
+    def set_pv(self, val):
+        self._pv = val
+
+    pv = property(get_pv, set_pv) 
---
+++ corps_code.py
@@ -0,0 +1,10 @@
+from _class_ import *
+
+Arc = Arme('Arc', 60, 80)
+Hache = Arme('Hache', 150, 20)
+
+Archer = ClasseJ(120, Arc, 20)
+Guerrier = ClasseJ(90, Hache, 100)
+
+
+kliklou=Joueur(0, Archer)
