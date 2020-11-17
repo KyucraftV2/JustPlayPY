f9f5155a0bb679b90857a0d6c10669f467bee1e7
Kyucraft <58594841+Kyucraft@users.noreply.github.com>
2020-11-17 14:20:27 +0100

    V1 finish
    End of the V1

---
+++ V1_finish/def_armes_classe.py
@@ -0,0 +1,16 @@
+from _class_ import *
+
+Arc = Arme('Arc', 60, 80)
+Hache = Arme('Hache', 150, 20)
+Epee = Arme('Epée', 100, 30)
+Lance = Arme('Lance', 80, 50)
+
+
+Archer = ClasseJ(120, Arc, 20)
+Berserker = ClasseJ(90, Hache, 100)
+Chevlier = ClasseJ(100, Epee, 70)
+Cavalier=ClasseJ(110, Lance, 45)
+
+kliklou=player(Archer,0)
+
+print(kliklou.get_pv())
