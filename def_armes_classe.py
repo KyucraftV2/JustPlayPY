756d714a7263d3afb266b6f66756e6120c3f836c
Kyucraft <42reki@gmail.com>
2020-11-21 14:51:24 +0100

    New Arme/Classe

--- def_armes_classe.py
+++ def_armes_classe.py
@@ -1,12 +1,14 @@
 from _class_ import *
 
 Arc = Arme('Arc', 60, 120)
+Arbalete = Arme('Arbaléte', 80, 100)
 Hache = Arme('Hache', 150, 20)
 Epee = Arme('Epée', 100, 30)
 Lance = Arme('Lance', 80, 50)
 
 
 Archer = ClasseJ(120, Arc, 10)
+Archer_Lourd = ClasseJ(100, Arbalete, 25)
 Berserker = ClasseJ(90, Hache, 100)
 Chevlier = ClasseJ(100, Epee, 70)
 Cavalier=ClasseJ(110, Lance, 45)
