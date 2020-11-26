daa9faca773d6ed73047cd747511c94e09c03e8f
Kyucraft <58594841+Kyucraft@users.noreply.github.com>
2020-11-26 10:40:22 +0100

    change

--- def_armes_classe.py
+++ def_armes_classe.py
@@ -1,14 +1,14 @@
 from _class_ import *
 
-Arc = Arme('Arc', 60, 120)
-Arbalete = Arme('Arbaléte', 80, 100)
-Hache = Arme('Hache', 150, 20)
-Epee = Arme('Epée', 100, 30)
-Lance = Arme('Lance', 80, 50)
+arc = Arme('Arc', 60, 120)
+arbalete = Arme('Arbaléte', 80, 100)
+hache = Arme('Hache', 150, 20)
+epee = Arme('Epée', 100, 30)
+lance = Arme('Lance', 80, 50)
 
 
-Archer = ClasseJ(120, Arc, 10)
-Archer_Lourd = ClasseJ(100, Arbalete, 25)
-Berserker = ClasseJ(90, Hache, 100)
-Chevlier = ClasseJ(100, Epee, 70)
-Cavalier=ClasseJ(110, Lance, 45)
+archer = ClasseJ(120, arc, 10)
+archer_Lourd = ClasseJ(100, arbalete, 25)
+berserker = ClasseJ(90, hache, 100)
+chevlier = ClasseJ(100, epee, 70)
+cavalier=ClasseJ(110, lance, 45)
