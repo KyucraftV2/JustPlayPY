510390f9457af136445d153afc7cc75356b121da
Kyucraft <58594841+Kyucraft@users.noreply.github.com>
2020-11-26 12:06:59 +0100

    hhh

---
+++ def_armes_classe.py
@@ -0,0 +1,14 @@
+from _class_ import *
+
+arc = Arme('Arc', 60, 120)
+arbalete = Arme('Arbaléte', 80, 100)
+hache = Arme('Hache', 150, 20)
+epee = Arme('Epée', 100, 30)
+lance = Arme('Lance', 80, 50)
+
+
+archer = ClasseJ(120, arc, 10)
+archer_Lourd = ClasseJ(100, arbalete, 25)
+berserker = ClasseJ(90, hache, 100)
+chevalier = ClasseJ(100, epee, 70)
+cavalier=ClasseJ(110, lance, 45)
